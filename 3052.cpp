d093a13b415ec4b39640525f802be1ce649109dc
2-changmin <101034582+2-changmin@users.noreply.github.com>
2022-07-09 16:10:54 +0900

    Update 3052.cpp

--- 3052.cpp
+++ 3052.cpp
@@ -5,14 +5,25 @@
 #include <math.h>
 
 int main() {
-	int a[9];
-	for (int i = 0; i < 8; i++) {
-		scanf("%d", a);
-	}
-	for (int i = 0; i < 8; i++) {
-		if (a[i + 1] == a[i] + 1) printf("ascending");
-		else if (a[i] == a[i] - 1) printf("descending");
-		else printf("mixed");
-	}
-	return 0;
+    int a;
+    int sum = 0;
+    int result = 0;
+    int n[10] = { 0, };
+    for (int i = 0; i <= 9; i++) {
+        scanf("%d", &a);
+        n[i] = a % 42;
+    }
+    for (int i = 0; i <= 9; i++) {
+        sum = 0;
+        for (int j = i + 1; j <= 9; j++) {
+            if (n[i] == n[j])
+                sum++;
+        }
+        if (sum == 0)result++;
+
+    }
+
+    printf("%d", result);
+
+    return 0;
 }
