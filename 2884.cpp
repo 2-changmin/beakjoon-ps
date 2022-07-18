002211aab62ecf4459813d15ac38b3a73db4f46c
2-changmin <101034582+2-changmin@users.noreply.github.com>
2022-07-19 04:56:24 +0900

    Update 2884.cpp

--- 2884.cpp
+++ 2884.cpp
@@ -7,17 +7,16 @@
 int main() {
 	int h, m;
 	scanf("%d %d", &h, &m);
-	if (h == 0) {
-		h = 24;
-	}
-	if (m >= 45) {
-		m = m - 45;
-	}
-	else if (m < 45) {
-		m = 45 - m;
-		m = 60 - m;
+	m = m - 45;
+	if (m < 0) {
+		m = m + 60;
 		h--;
 	}
+
+	if (h < 0) {
+		h += 24;
+	}
+	
 	printf("%d %d", h, m);
 
 	return 0;
