79f62a4f2af8ceeacce4b7fe40ad6fed5cdf5360
2-changmin <101034582+2-changmin@users.noreply.github.com>
2022-07-25 13:05:44 +0900

    Add files via upload

---
+++ 1152.cpp
@@ -0,0 +1,18 @@
+#pragma warning (disable:4996)
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+
+int main() {
+    int count = 0;
+    char a[1000004];
+    scanf("%[^\n]s", a);
+    if (a[0] != ' ') count++;
+    for (int i = 1; i < strlen(a); i++) {
+        if (a[i - 1] == ' ' && a[i] != ' ') count++;
+    }
+    printf("%d", count);
+    return 0;
+}
